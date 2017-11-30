0dfcacec5820a9b1ee8877b95a6f59d949282669
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2017-11-30 23:08:43 +0100

    filtering 1

--- ClientApp/app/components/pojazd-lista/pojazd-lista.component.ts
+++ ClientApp/app/components/pojazd-lista/pojazd-lista.component.ts
@@ -1,3 +1,4 @@
+import { KeyValuePair } from './../app/models/pojazd';
 import { PojazdService } from './../app/services/pojazd.service';
 import { Component, OnInit } from '@angular/core';
 import { Pojazd } from '../app/models/pojazd';
@@ -10,12 +11,32 @@ import { Pojazd } from '../app/models/pojazd';
 export class PojazdListaComponent implements OnInit {
 
   pojazdy: Pojazd[];
+  marki: KeyValuePair[];
+
+  allPojazdy: Pojazd[];
+
+  filter: any = {};
 
   constructor(private pojazdyService: PojazdService) { }
 
   ngOnInit() {
+    this.pojazdyService.getMarki()
+      .subscribe(marki => this.marki = marki);
+
     this.pojazdyService.getPojazdy()
-      .subscribe(pojazdy => this.pojazdy = pojazdy);
+      .subscribe(pojazdy => this.pojazdy = this.allPojazdy = pojazdy);
+  }
+
+  onFilterChange(){
+    var pojazdy = this.allPojazdy;
+
+    if (this.filter.markaId)
+      pojazdy = pojazdy.filter(p => p.marka.id == this.filter.markaId);
+    
+    if (this.filter.modelId)
+      pojazdy = pojazdy.filter(p => p.model.id == this.filter.modelId);
+
+    this.pojazdy = pojazdy;
   }
 
 }
